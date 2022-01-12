2625d0d6c5eecb58abc9e457a2fb0bc6e02ea236
TheSixLegendsOG <75628007+GamoupIndustries@users.noreply.github.com>
2022-01-12 09:00:14 -0500

    Create scratch-savesystem.js

---
+++ scratch-savesystem.js
@@ -0,0 +1,25 @@
+class SaveSystem {
+  getInfo() {
+    return {
+      id: "savesystem",
+      name: "Save and Load",
+      blocks: [
+        {
+          opcode: "save",
+          blockType: Scratch.BlockType.
+        },
+        {
+          opcode: "load"
+        }
+      ]
+    }
+  }
+  save() {
+    
+  }
+  load() {
+    
+  }
+}
+
+Scratch.extensions
