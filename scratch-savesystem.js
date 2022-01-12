72451b9444422ee507f65bd640aafa0310619c7b
TheSixLegendsOG <75628007+GamoupIndustries@users.noreply.github.com>
2022-01-12 11:52:36 -0500

    Update scratch-savesystem.js

--- scratch-savesystem.js
+++ scratch-savesystem.js
@@ -1,20 +1,30 @@
-class SaveSystem {
+class StrictEqualityExtension {
   getInfo() {
     return {
-      id: "savesystem",
-      name: "Save and Load",
+      id: 'strictequalityexample', // change this if you make an actual extension!
+      name: 'Strict Equality',
       blocks: [
         {
-          opcode: "save",
-          blockType: Scratch.BlockType.REPORTER,
-          text: "Save"
+          opcode: 'strictlyEquals',
+          blockType: Scratch.BlockType.BOOLEAN,
+          text: '[ONE] strictly equals [TWO]',
+          arguments: {
+            ONE: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'First value'
+            },
+            TWO: {
+              type: Scratch.ArgumentType.STRING,
+              defaultValue: 'Second value'
+            }
+          }
         }
       ]
-    }
+    };
   }
-  save() {
-    return "save";
+  strictlyEquals(args) {
+    // Note strict equality: Inputs must match exactly: in type, case, etc.
+    return args.ONE === args.TWO;
   }
 }
-
-Scratch.extensions.register(new SaveSystem());
+Scratch.extensions.register(new StrictEqualityExtension());
